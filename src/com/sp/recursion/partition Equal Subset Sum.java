38562fa74cfb2a2cc51936c6613212e8dee719fe
KATHROJI SAI YASHWANTH <saiyashwanth7554@gmail.com>
2025-12-04 12:32:29 +0530

    Update partition Equal Subset Sum.java

--- src/com/sp/recursion/partition Equal Subset Sum.java
+++ src/com/sp/recursion/partition Equal Subset Sum.java
@@ -1,30 +1,42 @@
 
 public class Main
 {
-    public boolean recursion(int arr[],int sum,int n,int k){
+    public boolean recursion(int arr[],int sum,int n,int k,int m[][]){
         if(sum==0){
             return true;
         }
         if(k==n){
             return false;
         }
+        if(m[k][sum]!=-1){
+            return (m[k][sum]==1)? true : false;
+        }
+        
         
         if(arr[k]<=sum){
-           if( recursion(arr,sum-arr[k],n,k+1)){
-               return true;
-           }
+           if(recursion(arr,sum-arr[k],n,k+1,m)){
+                m[k][sum]=1;
+                return true;
+            }
         }
-        return recursion(arr,sum,n,k+1);
-        
-        
+        m[k][sum]= (recursion(arr,sum,n,k+1,m)) ? 1 : 0;
+        return (m[k][sum]==1) ? true : false;
     }
-    public boolean partitionEqualSubsetSum(int arr[]){
+    public boolean partitionEqualSubsetSum(int[] arr) {
         int sum  = 0;
+
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
         }
+
         if(sum%2==0){
-            return recursion(arr,sum/2,arr.length,0);
+            int m[][] = new int[arr.length+1][sum/2+1];
+            for(int i=0;i<m.length;i++){
+                for(int j=0;j<m[0].length;j++){
+                    m[i][j]=-1;
+                }
+            }
+            return recursion(arr,sum/2,arr.length,0,m);
         }
         return false;
     }
